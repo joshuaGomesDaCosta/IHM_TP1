3cab2963d46ae3afa8b88e3ece77c420fa1a8ac8
Joshua Gomes Da Costa <joshua.gomesdacosta@heig-vd.ch>
2020-11-05 13:43:25 +0100

    recommit

--- post_it/src/index.js
+++ post_it/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Canvas from './composants/Canvas'
+import Menu from './composants/Menu'
+import Page from './composants/Page'
+import FormNewPostIt from './composants/FormNewPostIt'
+
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(<Canvas />, document.getElementById('menu'));
-//ReactDOM.render(< />, document.getElementById('page'));
-//ReactDOM.render(< />, document.getElementById('fNewPostIt'));
+ReactDOM.render(<Menu />, document.getElementById('menu'));
+ReactDOM.render(<Page />, document.getElementById('page'));
+ReactDOM.render(<FormNewPostIt />, document.getElementById('fNewPostIt'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
